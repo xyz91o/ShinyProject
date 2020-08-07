f423fd7421cab85d0086fdd476eef4859a15b84b
Balpree <68390092+Balpree@users.noreply.github.com>
2020-08-07 15:43:16 +0200

    UI and server shiny project

--- ui.R
+++ ui.R
@@ -1,33 +1,62 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(HistData)
+data(GaltonFamilies)
+library(dplyr)
+library(ggplot2)
 
-# Define UI for application that draws a histogram
-shinyUI(fluidPage(
+# 1st step: pass inches to cm
+gf <- GaltonFamilies
+gf <- gf %>% mutate(father=father*2.54,
+                    mother=mother*2.54,
+                    childHeight=childHeight*2.54)
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
+# linear model
+model1 <- lm(childHeight ~ father + mother + gender, data=gf)
 
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-            plotOutput("distPlot")
-        )
+shinyServer(function(input, output) {
+  output$pText <- renderText({
+    paste("Father's height is",
+          strong(round(input$inFh, 1)),
+          "cm, and mother's height is",
+          strong(round(input$inMh, 1)),
+          "cm, then:")
+  })
+  output$pred <- renderText({
+    df <- data.frame(father=input$inFh,
+                     mother=input$inMh,
+                     gender=factor(input$inGen, levels=levels(gf$gender)))
+    ch <- predict(model1, newdata=df)
+    kid <- ifelse(
+      input$inGen=="female",
+      "Daugther",
+      "Son"
     )
-))
+    paste0(em(strong(kid)),
+           "'s predicted height is going to be around ",
+           em(strong(round(ch))),
+           " cm"
+    )
+  })
+  output$Plot <- renderPlot({
+    kid <- ifelse(
+      input$inGen=="female",
+      "Daugther",
+      "Son"
+    )
+    df <- data.frame(father=input$inFh,
+                     mother=input$inMh,
+                     gender=factor(input$inGen, levels=levels(gf$gender)))
+    ch <- predict(model1, newdata=df)
+    yvals <- c("Father", kid, "Mother")
+    df <- data.frame(
+      x = factor(yvals, levels = yvals, ordered = TRUE),
+      y = c(input$inFh, ch, input$inMh))
+    ggplot(df, aes(x=x, y=y, color=c("red", "green", "blue"), fill=c("red", "green", "blue"))) +
+      geom_bar(stat="identity", width=0.5) +
+      xlab("") +
+      ylab("Height (cm)") +
+      theme_minimal() +
+      theme(legend.position="none")
+  })
+})
+
